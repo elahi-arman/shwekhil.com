eabfd25bad17022800cefbaf1886b4c93ebe5275
Arman Elahi <code@elahi-arman.me>
2020-01-22 00:16:17 -0800

    Fixes accomodations

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,30 @@
 const Shwekhil = {
-    elements: 
+    elements: {
+        bookAccomodationsButton: document.querySelector('.accomodations-button')
+    },
+    handlers: {
+        redirectToHotel: () => {
+            window.open(Shwekhil.constants.BOOK_HOTEL_LINK, '_blank');            
+        }
+    },
+    constants: {
+        BOOK_HOTEL_LINK: 'https://www.marriott.com/event-reservations/reservation-link.mi?id=1577987940847&key=GRP&app=resvlink'
+    },
+    init() {
+        Shwekhil.elements.bookAccomodationsButton.onclick = Shwekhil.handlers.redirectToHotel;
+
+        if (!!DB) {
+            DB.init();
+        } else {
+            console.error('No DB object found');
+        }
+        
+        if (!!RSVPForm) {
+            RSVPForm.init()
+        } else {
+            console.error('No RSVPForm object found')
+        }
+    }
 }
+
+Shwekhil.init()
